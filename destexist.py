357bc9aa30cc4e1e6ffb1945674fb25e1c29627e
vivek <vivek254258@gmail.com>
2020-08-23 16:48:24 +0530

    DestExist.py was modified

--- destexist.py
+++ destexist.py
@@ -13,6 +13,7 @@ root.geometry("200x200")
 b1 = Button(root, text="Button 1")
 b1.pack(fill = BOTH,expand=True)
 
+print(" in Middle of the program")
 b2 = Button(root, text="Button 2",command = lambda :destroy(b1)).pack(fill = BOTH,expand =True)
 
 b3 = Button(root,text = "Button 3",command = lambda :exist(b1)).pack(fill = BOTH,expand =True)
